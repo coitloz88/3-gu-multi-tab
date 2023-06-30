e44180388b462417063bfe9775d7675852d165bc
Hyemin Lee <cotloz88@gmail.com>
2023-06-30 19:42:41 +0900

    bind data to recyclerview in contact fragment

--- app/src/main/java/com/example/week1/ContactFragment.kt
+++ app/src/main/java/com/example/week1/ContactFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.week1.adapter.ContactsAdapter
+import com.example.week1.data.Contact
+import com.example.week1.databinding.FragmentContactBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +25,8 @@ class ContactFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private var _binding: FragmentContactBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -32,9 +38,23 @@ class ContactFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false)
+        _binding = FragmentContactBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val contactList = ArrayList<Contact>()
+        contactList.add(Contact(1, "유저1", R.drawable.image_contact_manggom))
+        contactList.add(Contact(2, "유저2", null))
+
+        binding.rvContacts.layoutManager = LinearLayoutManager(activity)
+        binding.rvContacts.adapter = ContactsAdapter(contactList)
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
