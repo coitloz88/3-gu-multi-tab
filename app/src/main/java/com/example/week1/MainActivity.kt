45db2649d26a639a89fc92a165e1f9bcec143b25
Hyemin Lee <cotloz88@gmail.com>
2023-06-30 12:59:04 +0900

    add event listener for bottom navigation tab

--- app/src/main/java/com/example/week1/MainActivity.kt
+++ app/src/main/java/com/example/week1/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.week1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+    private val contactFragment by lazy { ContactFragment() }
+    private val galleryFragment by lazy { GalleryFragment() }
+    private val freeFragment by lazy { FreeFragment() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // initial fragment 설정
+        supportFragmentManager.beginTransaction().add(R.id.fl_container, contactFragment).commit()
+
+        // bottom navigation item 탭 이벤트 리스너 설정
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bnv_main)
+        bottomNavigationView.run {
+            setOnItemSelectedListener {
+                when(it.itemId) {
+                    R.id.contact_tab -> {
+                        changeFragment(contactFragment)
+                    }
+                    R.id.gallery_tab -> {
+                        changeFragment(galleryFragment)
+                    }
+                    R.id.free_tab -> {
+                        changeFragment(freeFragment)
+                    }
+                }
+                true
+            }
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fl_container, fragment).commit()
     }
 }
