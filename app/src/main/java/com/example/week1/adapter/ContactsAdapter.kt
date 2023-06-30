9d7ab7600578767e40adaa6253bbd5a3d5bdd3a6
Hyemin Lee <cotloz88@gmail.com>
2023-06-30 19:41:33 +0900

    implement contact adapter using `RecyclerviewAdapter`

--- app/src/main/java/com/example/week1/adapter/ContactsAdapter.kt
+++ app/src/main/java/com/example/week1/adapter/ContactsAdapter.kt
@@ -1,67 +1,37 @@
 package com.example.week1.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.week1.R
 import com.example.week1.data.Contact
+import com.example.week1.databinding.ContactItemBinding
 
-class ContactsAdapter(private val onClick: (Contact) -> Unit):
-    ListAdapter<Contact, ContactsAdapter.ContactViewHolder>(ContactDiffCallback) {
+class ContactsAdapter(val contactList: ArrayList<Contact>) :
+    RecyclerView.Adapter<ContactsAdapter.Holder>() {
 
-    class ContactViewHolder(itemView: View, val onClick: (Contact) -> Unit):
-        RecyclerView.ViewHolder(itemView) {
-        private val nameTextView: TextView = itemView.findViewById(R.id.contact_name)
-        private val imageImageView: ImageView = itemView.findViewById(R.id.contact_image)
-        private var currentContact: Contact? = null
+    inner class Holder(val binding: ContactItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val name = binding.contactName
+        val image = binding.contactImage
+    }
 
-        init {
-            itemView.setOnClickListener {
-                currentContact?.let {
-                    onClick(it)
-                }
-            }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = ContactItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return Holder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return contactList.size
+    }
+
+    override fun onBindViewHolder(holder: ContactsAdapter.Holder, position: Int) {
+        holder.name.text = contactList[position].name
+
+        val image = contactList[position].image
+        if(image != null){
+            holder.image.setImageResource(image)
+        } else {
+            holder.image.setImageResource(R.drawable.image_contact_default)
         }
-
-        // name과 image를 binding
-        fun bind(contact: Contact) {
-            currentContact = contact
-
-            nameTextView.text = contact.name
-            if (contact.image != null) {
-                imageImageView.setImageResource(contact.image)
-            } else {
-                imageImageView.setImageResource(R.drawable.image_contact_default)
-            }
-        }
-    }
-
-    // creates and inflates view and return ContactViewHolder
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): ContactsAdapter.ContactViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
-        return ContactViewHolder(view, onClick)
-    }
-
-    override fun onBindViewHolder(holder: ContactsAdapter.ContactViewHolder, position: Int) {
-        val contact = getItem(position)
-        holder.bind(contact)
-    }
-}
-
-object ContactDiffCallback: DiffUtil.ItemCallback<Contact>() {
-    override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
-        return oldItem == newItem
-    }
-
-    override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean {
-        return oldItem.id == newItem.id
     }
 }
