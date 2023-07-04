d7b92559493fc48a3cafd72d176e5939c01f684d
dyheo619 <dyheo619@gmail.com>
2023-07-04 17:52:26 +0900

    wip

--- app/src/main/java/com/example/week1/CurrencyFragment.kt
+++ app/src/main/java/com/example/week1/CurrencyFragment.kt
@@ -1,5 +1,7 @@
 package com.example.week1
 
+
+//import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,6 +10,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.TextView
 import com.example.week1.databinding.FragmentCurrencyBinding
 import com.example.week1.retrofit.CurrencyService
 import com.example.week1.retrofit.RetrofitClass
@@ -39,8 +43,17 @@ class CurrencyFragment : Fragment() {
         _binding = FragmentCurrencyBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        val adapterFrom = ArrayAdapter.createFromResource(requireContext(), R.array.currency_code, android.R.layout.simple_spinner_dropdown_item)
-        val adapterTo = ArrayAdapter.createFromResource(requireContext(), R.array.currency_code, android.R.layout.simple_spinner_dropdown_item)
+        val adapterFrom = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.currency_code,
+            android.R.layout.simple_spinner_dropdown_item
+        )
+        val adapterTo = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.currency_code,
+            android.R.layout.simple_spinner_dropdown_item
+        )
+
 
         binding.spinnerFrom?.let { spinner ->
             spinner.adapter = adapterFrom
@@ -51,7 +64,6 @@ class CurrencyFragment : Fragment() {
                     position: Int,
                     id: Long
                 ) {
-                    Log.d(TAG, "selected from code: " + spinner.selectedItem.toString())
                 }
 
                 override fun onNothingSelected(p0: AdapterView<*>?) {
@@ -69,9 +81,7 @@ class CurrencyFragment : Fragment() {
                     position: Int,
                     id: Long
                 ) {
-                    Log.d(TAG, "selected to code: " + spinner.selectedItem.toString())
                 }
-
                 override fun onNothingSelected(p0: AdapterView<*>?) {
                 }
 
@@ -79,10 +89,26 @@ class CurrencyFragment : Fragment() {
         }
 
         binding.confirmButton.setOnClickListener {
-            calculateAmountResult("usd", "krw", 1.0)
-        }
+            val hintString = binding.etAmount.getText().toString()
 
-        return view
+
+            val hint: Double? = hintString.toDoubleOrNull()
+
+
+            if (binding.spinnerFrom != null && binding.spinnerTo != null && hint != null && hint >= 0) {
+                calculateAmountResult(
+                    binding.spinnerFrom.selectedItem.toString(),
+                    binding.spinnerTo.selectedItem.toString(),
+                    hint
+                )
+            }
+
+            else {
+                onDestroyView()
+            }
+
+        }
+        return binding.root
     }
 
     override fun onDestroyView() {
@@ -92,12 +118,13 @@ class CurrencyFragment : Fragment() {
 
     private fun calculateAmountResult(from: String, to: String, inputAmount: Double) {
         val apiService = RetrofitClass.getInstance().create(CurrencyService::class.java)
-        apiService.getExchangeRate("$from/$to")?.enqueue(object: Callback<String>{
+        apiService.getExchangeRate("$from/$to")?.enqueue(object : Callback<String> {
             override fun onResponse(call: Call<String>, response: Response<String>) {
-                if(response.isSuccessful) {
+                if (response.isSuccessful) {
                     val jsonObject = JSONObject(response.body().toString())
                     val rate = jsonObject.getDouble(to)
-                    binding.tvResult.text = (rate * inputAmount).toString()
+                    val result = rate * inputAmount
+                    binding.tvResult.setText(String.format("%.2f", result))
                 }
             }
 
