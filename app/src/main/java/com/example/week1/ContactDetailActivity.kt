924363e45b4338d49959f11865d80439390a8dbd
Hyemin Lee <cotloz88@gmail.com>
2023-07-04 15:02:15 +0900

    call, message floating action button 활성화

--- app/src/main/java/com/example/week1/ContactDetailActivity.kt
+++ app/src/main/java/com/example/week1/ContactDetailActivity.kt
@@ -1,8 +1,14 @@
 package com.example.week1
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.week1.databinding.ActivityContactDetailBinding
 import com.example.week1.utils.getDrawableFromUri
 
@@ -23,12 +29,36 @@ class ContactDetailActivity : AppCompatActivity() {
         binding.tvName.text = name
         binding.tvPhoneNumber.text = number
 
+        Log.d("Phone Number", number.toString())
+
         if(imageUri != null) {
             binding.ivProfile.setImageDrawable(getDrawableFromUri(this, imageUri))
         } else {
             binding.ivProfile.setImageResource(R.drawable.image_contact_default)
         }
 
+        checkPermissions()
+
+        binding.fabCall.setOnClickListener{
+            val callIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$number"))
+            startActivity(callIntent)
+        }
+
+        binding.fabMessage.setOnClickListener{
+            val messageIntent = Intent(Intent.ACTION_VIEW, Uri.parse("sms:$number"))
+            startActivity(messageIntent)
+        }
+    }
+
+    private fun checkPermissions() {
+        val statusCall = ContextCompat.checkSelfPermission(this, "android.permission.CALL_PHONE")
+        if(statusCall == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf<String>("android.permission.CALL_PHONE"), 100)
+        }
+        val statusSms = ContextCompat.checkSelfPermission(this, "android.permission.SEND_SMS")
+        if(statusSms == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf<String>("android.permission.SEND_SMS"), 100)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
