a6c4455a8577231422f084ade401afe3a9b29bd2
Hyemin Lee <cotloz88@gmail.com>
2023-07-04 11:45:45 +0900

    연락처 클릭시 contact detail page로 넘어가는 기능 추가

--- app/src/main/java/com/example/week1/ContactDetailActivity.kt
+++ app/src/main/java/com/example/week1/ContactDetailActivity.kt
@@ -2,15 +2,46 @@ package com.example.week1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import com.example.week1.databinding.ActivityContactDetailBinding
 
 class ContactDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityContactDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact_detail)
+        binding = ActivityContactDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val id = intent.getIntExtra("id", 0)
+
+        if(id == 0) {
+            Log.e(Companion.TAG, "id is 0!!")
+            finish()
+        }
+
+        Log.d(TAG, "id: $id")
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
         finish()
+    }
+
+    companion object {
+        private const val TAG = "ContactDetailActivity"
     }
 }
